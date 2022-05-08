da7866b0610370926a1b53a0a33732bc0d0e71bc
Cort Widlowski <cortozld@gmail.com>
2022-05-08 10:28:41 +0200

    May 08 Codewars

---
+++ 8-kyu/You-cant-code-under-pressure-1.js
@@ -0,0 +1,36 @@
+/*
+Tasks:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+/*
+Examples:
+
+*/
+
+//My Result:
+
+function doubleInteger(i) {
+  return i * 2
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+Test Examples:
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(doubleInteger(2), 4);
+  });
+});
+
+
+*/
