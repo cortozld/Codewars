e9597ad9a50aa286e143f46025c97a47f45ec9a7
Cort Widlowski <cortozld@gmail.com>
2022-05-21 11:24:42 +0200

    May 20 and May 21 2022 Codewars

---
+++ 8-kyu/Super-duper-easy.js
@@ -0,0 +1,42 @@
+/*
+Tasks:
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+*/
+
+/*
+Examples:
+
+*/
+
+//My Result:
+
+function problem(x){
+  return typeof(x) == "number" ? x * 50 + 6 : "Error"
+}
+
+/*
+Test Examples:
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(problem("hello"), "Error");
+    assert.strictEqual(problem(1), 56);
+    assert.strictEqual(problem(5), 256);
+    assert.strictEqual(problem(0), 6);
+    assert.strictEqual(problem(1.2), 66);
+    assert.strictEqual(problem(3), 156);
+    assert.strictEqual(problem("RyanIsCool"), "Error");
+    assert.strictEqual(problem(-3), -144);
+    assert.strictEqual(problem(""), "Error");
+    assert.strictEqual(problem(0.03), 7.5);
+  })
+})
+
+
+*/
